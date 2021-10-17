b45e88b2a9ca0a7bad213c625f7f7026b4a54c06
Santiago L Martinez <slm17slm17@gmail.com>
2021-10-16 23:29:12 -0300

    Subiendo archivos
    commit 01 hay un error en el codigo

---
+++ businessLogic.js
@@ -0,0 +1,17 @@
+//aqui programaremos objetos y funcionabilidad del sistema
+
+//creamos una variable para recorrer toda la lista de personas en un arreglo
+var personList = [];
+
+//creamos funcion para agregar personas a la lista
+function addPersonToSystem(pid, pname, pbirthday, pemail) {
+
+    var newFriend = {
+        id: pId,
+        name: pName,
+        birthday: pBirthday,
+        email: pEmail
+    };
+    console.log(newFriend);
+    personList.push(newFriend);
+}
